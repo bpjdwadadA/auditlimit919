5cf291acefa7e62ffa7ba9f317a30583cdbdf77c
bpjdwadadA <139862703+bpjdwadadA@users.noreply.github.com>
2024-09-19 15:00:56 +0800

    消息添加中文

--- api/result.go
+++ api/result.go
@@ -6,7 +6,7 @@ var (
 		"detail": {
 		  "clears_in": 252,
 		  "code": "model_cap_exceeded",
-		  "message": "You have sent too many messages to the model. Please try again later."
+		  "message": "You have sent too many messages to the model. Please try again later.您向模型发送了过多的消息，已经超过了允许的请求频率限制."
 		}
 	  }
 	`
@@ -15,7 +15,7 @@ var (
 	{
 		"detail": {
 		  "code": "flagged_by_moderation",
-		  "message": "This content may violate [OpenAI Usage Policies](https://openai.com/policies/usage-policies)."
+		  "message": "This content may violate [OpenAI Usage Policies](https://openai.com/policies/usage-policies).此内容可能违反 OpenAI [OpenAI使用政策](https://openai.com/policies/usage-policies)"
 		}
 	}
 	`
